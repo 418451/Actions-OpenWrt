6dcaffdd3f05c853a7e60f3e8f9791debd447bcc
418451 <30517580+418451@users.noreply.github.com>
2024-09-13 10:48:20 +0800

    Delete diy-part2.sh

--- diy-part2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# --------------------------------------------------------
-# Script to compile and create files for each openwrt
-# --------------------------------------------------------
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.251/g' package/base-files/files/bin/config_generate
