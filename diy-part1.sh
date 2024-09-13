6a61952780cbdc66315ccd6bd2a5990c58b7276f
418451 <30517580+418451@users.noreply.github.com>
2024-09-13 10:32:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,3 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
